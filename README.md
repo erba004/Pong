afc1eaefdf56249d949fbf5df61acd91316b282b
“erba004” <erlendjbakke@gmail.com>
2023-11-02 12:46:04 +0100

    lagde treff funksjon, variabler og reset

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Prosjektoppgave_uke41-43
+
+# hva tenker du om fargene?
+# hva tenker du om formene?
+# hvor lett var det å forstå spillet?
+# hva tenker du om hvor fort ting flytter seg
+# hva mer kan jeg legge til?
+# hva kan jeg forbedre?
+
+
+
+- likte farger
+- likte former
+- lett å forstå
+- likte hastigheten
+
+- kan legge til bakgrunnsmusikk
+- 
--- main.js
+++ main.js
@@ -5,8 +5,21 @@ const c = canvas.getContext('2d')
 canvas.width = 1440
 canvas.height = 787
 
+const increaseX = 1
+const increaseY = 0.5
+const behind = 25
+const ballWidth = 30
+const basePositionY = (canvas.height/2) - (ballWidth/2)
+const basePositionX = (canvas.width/2) - (ballWidth/2) 
+let baseSpeedX = 4
+let baseSpeedY = 2
+let score1 = 0
+let score2 = 0
+
+
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+//creates a class to make it easier to make the players
 class Sprite {
     constructor({position, velocity, color = 'white'}) {
         this.position = position
@@ -37,12 +50,13 @@ class Sprite {
 
 }
 
+//creates a class for the ball
 class BallClass {
     constructor({position, velocity, color = 'white'}) {
         this.position = position
         this.velocity = velocity
-        this.width = 30
-        this.height = 30
+        this.width = ballWidth
+        this.height = ballWidth
         this.lastkey
         this.color = color
     }
@@ -52,14 +66,31 @@ class BallClass {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
+    //updates and checks for movement every tick
     update() {
         this.draw()
         this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
+        this.position.y += this.velocity.y    
+        
+        if (this.velocity.y >= 10) {
+            this.velocity.y = 10
+        } else if (this.velocity.y <= -10) {
+            this.velocity.y = -10
+        }
+        
+        if (this.velocity.x >= 20) {
+            this.velocity.x = 20
+        } else if (this.velocity.x <= -20) {
+            this.velocity.x = -20
+        }
+
     }
+
+
 
 }
 
+//creates player1
 const player1 = new Sprite({
     position: {
         x: 150,
@@ -73,6 +104,7 @@ const player1 = new Sprite({
 
 })
 
+//creates player2
 const player2 = new Sprite({
     position: {
         x: canvas.width - 150,
@@ -86,20 +118,23 @@ const player2 = new Sprite({
 
 })
 
+//creates the ball
 const ball = new BallClass({
     position: {
-        x: (canvas.width/2) - 15,
-        y: (canvas.height/2) - 15
+        x: basePositionX,
+        y: basePositionY
     },
     velocity: {
-        x: 0,
-        y: 0
+        x: baseSpeedX,
+        y: baseSpeedY
     }
 })
 
 console.log(player1)
 console.log(player2)
 
+//this is to make sure the players dont continue to move 
+//after the buttons are pressed
 const keys =  {
     w: {
         pressed: false
@@ -115,11 +150,27 @@ const keys =  {
     }
 }
 
-function rectangularCollision({rectangle1, rectangle2}) {
-
-
+function score(side) {
+    if (side < canvas.width/2) {
+        score1 += 1
+    } else {
+        score2 += 1
+    }
 }
 
+function reset() {
+    ball.velocity.x = baseSpeedX
+    ball.velocity.y = baseSpeedY
+    ball.position.x = basePositionX
+    ball.position.y = basePositionY
+}
+
+function hitPlayer(amountX) {
+    ball.velocity.x *= -1
+    ball.velocity.x += amountX
+}
+
+//main gameplay loop
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -146,6 +197,47 @@ function animate() {
     }
 
 
+    //spiller 1 treff
+    if (
+        ball.position.x <= player1.position.x + player1.width &&
+        ball.position.y + ball.height >= player1.position.y &&
+        ball.position.y <= player1.position.y + player1.height &&
+        ball.position.x >= player1.position.x
+    ) {
+        hitPlayer(increaseX)
+
+    }
+
+    //spiller 1 treff topp
+
+
+    //spiller 2 treff
+    if (
+        ball.position.x + ball.width >= player2.position.x &&
+        ball.position.y + ball.height >= player2.position.y &&
+        ball.position.y <= player2.position.y + player2.height &&
+        ball.position.x + ball.width <= player2.position.x + player2.width
+    ) {
+        hitPlayer(-increaseX)
+        console.log(ball)
+    }
+
+    if (
+        ball.position.y <= 0 || ball.position.y + ball.height >= canvas.height
+    ) {
+        ball.velocity.y *= -1
+    }
+
+    if (
+        ball.position.x <= behind || ball.position.x + ball.width >= canvas.width - behind
+    ) {
+        score(ball.position.x)
+        reset()
+        console.log(score1)
+        console.log(score2)
+        
+    }
+
 }
 
 animate()
@@ -153,6 +245,7 @@ animate()
 console.log(player2)
 console.log(ball)
 
+//player movement
 window.addEventListener(('keydown'), (event) => {
     switch (event.key) {
         case 'w':
@@ -174,6 +267,7 @@ window.addEventListener(('keydown'), (event) => {
     }
 })
 
+//checks when the player is not pressing the movemnt keys anymore
 window.addEventListener(('keyup'), (event) => {
     switch (event.key) {
         case 'w':
