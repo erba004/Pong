2271d94435d85007fc4e7119a07e74ab76c021e1
“erba004” <erlendjbakke@gmail.com>
2024-06-06 12:28:49 +0200

    test

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else  echo("<br>"+"connected");
+} else  echo("connected");
 
 // Data to be inserted
 $name = "John Doe";
